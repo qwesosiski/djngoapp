b425f78806f474d4ad3713cc66df0d530f963ff9
Студент группы IS <is@sielom.local>
2025-11-27 12:36:19 +0500

    beta4

--- library/urls.py
+++ library/urls.py
@@ -1,3 +1,4 @@
+# urls.py
 from django.urls import path
 from . import views
 
@@ -5,4 +6,5 @@ urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('books/', views.book_list, name='book_list'),
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('author/<int:author_id>/', views.author_detail, name='author_detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,7 @@
+# views.py
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-from .models import Book
+from .models import Book, Author
 
 def book_list(request):
     books = Book.objects.all()
@@ -15,3 +16,11 @@ def book_list(request):
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     return render(request, 'book_detail.html', {'book': book})
+
+def author_detail(request, author_id):
+    author = get_object_or_404(Author, id=author_id)
+    books = Book.objects.filter(author=author)
+    return render(request, 'author_detail.html', {
+        'author': author,
+        'books': books
+    })
